03cc9f2c65523f8fb8ae0d1cc0618ec2d3513fb7
BilalAhmed369 <chbilalahmed369@gmail.com>
2025-09-17 00:17:15 +0500

    Add files via upload

---
+++ 1. Select Statment.sql
@@ -0,0 +1,21 @@
+SELECT * 
+FROM parks_and_recreation.employee_demographics;
+
+
+SELECT first_name,
+ age,
+ birth_date,
+ age, 
+ age + 10
+FROM parks_and_recreation.employee_demographics;
+
+
+
+SELECT distinct gender
+FROM parks_and_recreation.employee_demographics;
+
+
+
+
+
+
